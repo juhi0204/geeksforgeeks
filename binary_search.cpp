da9d809d4c66fac88f78d781325a8065d4feb953
juhi0204 <60579754+juhi0204@users.noreply.github.com>
2022-10-12 00:33:31 +0530

    Add files via upload

---
+++ binary_search.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    int binarysearch(int arr[], int n, int k)
+    {
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        int mid = low + (high - low) / 2;
+        while (low <= high)
+        {
+            if (arr[mid] == k)
+            {
+                ans = mid;
+                break;
+            }
+            else if (k < arr[mid])
+            {
+                high = mid - 1;
+            }
+            else if (k > arr[mid])
+            {
+                low = mid + 1;
+            }
+            mid = low + (high - low) / 2;
+        }
+        return ans;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
